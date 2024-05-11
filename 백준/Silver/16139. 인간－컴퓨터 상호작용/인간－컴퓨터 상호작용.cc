977f605f8bae1c703e340468f925a983465d894b
sasasa6391 <sasasa6391@gmail.com>
2024-05-11 17:59:46 +0900

    [Silver I] Title: 인간-컴퓨터 상호작용, Time: 112 ms, Memory: 43364 KB, Score: 100 point -BaekjoonHub

--- 백준/Silver/16139. 인간－컴퓨터 상호작용/인간－컴퓨터 상호작용.cc
+++ 백준/Silver/16139. 인간－컴퓨터 상호작용/인간－컴퓨터 상호작용.cc
@@ -1,7 +1,7 @@
 #include <bits/stdc++.h>
 using namespace std;
 typedef long long ll;
-const ll MX = 101010;
+const ll MX = 201010;
 const ll INF = 1e18;
 #define FIO cin.tie(0); cout.tie(0); ios_base::sync_with_stdio(false)
 ll n, k, cnt[MX][26];
