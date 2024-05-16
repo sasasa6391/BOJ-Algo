237c6678baa7e19c15b394a169ec4f359a7ba917
sasasa6391 <sasasa6391@gmail.com>
2024-05-16 14:12:21 +0900

    [Bronze V] Title: 사칙연산, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/10869. 사칙연산/사칙연산.cc
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+const ll MX = 201010;
+const ll INF = 1e18;
+#define FIO cin.tie(0); cout.tie(0); ios_base::sync_with_stdio(false)
+int main()
+{
+	FIO;
+
+	int A, B;
+	cin >> A >> B;
+
+
+	cout << A + B << "\n";
+	cout << A - B << "\n";
+	cout << A * B << "\n";
+	cout << A / B << "\n";
+	cout << A % B << "\n";
+}
