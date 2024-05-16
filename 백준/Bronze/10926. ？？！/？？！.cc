7895f9308d01f41b76cf1cae5204a4308fcdd9bf
sasasa6391 <sasasa6391@gmail.com>
2024-05-16 14:12:58 +0900

    [Bronze V] Title: ??!, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/10926. ？？！/？？！.cc
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+const ll MX = 201010;
+const ll INF = 1e18;
+#define FIO cin.tie(0); cout.tie(0); ios_base::sync_with_stdio(false)
+int main()
+{
+	FIO;
+
+	string s;
+
+	cin >> s;
+	cout << s << "??!";
+}
